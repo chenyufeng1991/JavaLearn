e6e73c0a1a0efc5e1bca94946abc0c37d0504f98
陈宇峰 <yufengcode@gmail.com>
2016-12-10 21:39:47 +0800

    +

---
+++ src/com/chenyufeng/lesson01/Test06.java
@@ -0,0 +1,17 @@
+package com.chenyufeng.lesson01;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		
+		//Math中方法的测试
+		
+		//floor()方法会截取整数部分，直接去掉小数部分
+		System.out.println("10 / 3 = " + Math.floor(10.0 / 3));
+		
+		//对小数点第一位进行四舍五入
+		System.out.println("3.73小数点后第一位四舍五入" + Math.round(3.73));
+
+	}    
+
+}
