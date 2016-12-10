5309dca8aa81985ad62d891b6204adb438a94b6f
陈宇峰 <yufengcode@gmail.com>
2016-12-10 20:24:35 +0800

    捕获args

---
+++ src/com/chenyufeng/lesson01/Test04.java
@@ -0,0 +1,15 @@
+package com.chenyufeng.lesson01;
+
+public class Test04 {
+
+	/*
+	 * 可以配置args输入参数，从而可以捕获该值。
+	 * 进入Run as-->Run Configuratons-->Arguments-->Pragram Argument中去配置args这个String字符串数组。
+	 * 使用空格分隔
+	 * */
+	public static void main(String[] args) {
+		System.out.println("hello" + args[1]);
+
+	}
+
+}
