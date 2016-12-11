a9a00d029900b34c85f485268da7e2dc5088c2e7
陈宇峰 <yufengcode@gmail.com>
2016-12-11 12:54:28 +0800

    array

---
+++ src/com/chenyufeng/lesson02/Test01.java
@@ -0,0 +1,20 @@
+package com.chenyufeng.lesson02;
+
+public class Test01 {
+	
+	public static void main(String[] args) {
+		int array[]; //数组的声明
+		array = new int[3]; //为数组开辟内存空间，实例化 
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+		
+	}
+
+}
